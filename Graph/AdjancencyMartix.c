2f2b25cc582b4cbe06df516270577b4675e6d2e8
MtJZd <white_yasha@163.com>
2018-11-28 22:30:19 +0800

    Update AdjancencyMartix.c

--- Graph/AdjancencyMartix.c
+++ Graph/AdjancencyMartix.c
@@ -8,6 +8,28 @@
 
 #include "AdjacencyMartix.h"
 
+//Union Find
+UnionFind CreateUnionFind()
+{
+    UnionFind *uf = (UnionFind*)malloc(sizeof(UnionFind));
+    for (int i = 0; i < MAX_SIZE; i++) (*uf).fa[i] = i;
+    return *uf;
+}
+int FindFather(UnionFind *uf, int x)
+{
+    return (*uf).fa[x] == x ? x : ((*uf).fa[x] = FindFather(uf, (*uf).fa[x]));
+}
+void MergeFather(UnionFind *uf, int x, int y)
+{
+    int fx = FindFather(uf, x), fy = FindFather(uf, y);
+    if (fx != fy) (*uf).fa[fx] = fy;
+}
+//
+int cmp(const void *a, const void *b)
+{
+    return (*(edge *)a).w - (*(edge *)b).w;
+}
+
 MartixGraph CreateMartixGraph(int size)                //create a new empty graph
 {
     MartixGraph *g = (MartixGraph*)malloc(sizeof(MartixGraph));
@@ -21,7 +43,7 @@ void AddEdge_Martix(MartixGraph *g, int u, int v, int w)               //add a n
 {
     (*g).map[u][v] = (*g).map[v][u] = w;
 }
-int ShortestPath(MartixGraph g, int u, int v)              //shortest path between u and v
+int ShortestPath_Martix(MartixGraph g, int u, int v)              //shortest path between u and v
 {
     //Dijkstra Algorithm
     int dis[MAX_SIZE], vis[MAX_SIZE];
@@ -37,4 +59,25 @@ int ShortestPath(MartixGraph g, int u, int v)              //shortest path betwe
                 dis[j] = dis[p] + g.map[p][j];
     }
     return dis[v];
+}
+int MinCostSpanTree(MartixGraph g)             //minimum cost spaning tree
+{
+    //Kruskal Algorithm
+    UnionFind uf = CreateUnionFind();
+    edge e[g.size * g.size];
+    int tot = 0, res = 0;
+    for (int i = 0; i < g.size; i++)
+        for (int j = 0; j < g.size; j++)
+            if (g.map[i][j] != INF) e[tot++] = (edge){i, j, g.map[i][j]};
+    qsort(e, tot, sizeof(edge), cmp);
+    for (int i = 0; i < tot; i++) {
+        if (FindFather(&uf, e[i].u) != FindFather(&uf, e[i].v)) {
+            MergeFather(&uf, e[i].u, e[i].v);
+            res += e[i].w;
+        }
+    }
+    for (int i = 0; i < g.size; i++)
+        if (FindFather(&uf, i) != FindFather(&uf, 0))
+            return -1;              //graph disconnected
+    return res;
 }

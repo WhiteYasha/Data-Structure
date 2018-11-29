6a16dc9c4f9910ef4fb195e0005f53fcbc7302f8
MtJZd <white_yasha@163.com>
2018-11-29 09:09:11 +0800

    Update AdjacencyList.c

--- Graph/AdjacencyList.c
+++ Graph/AdjacencyList.c
@@ -32,13 +32,14 @@ ListGraph CreateListGraph()               //create a new empty list graph
 {
     ListGraph *lg = (ListGraph*)malloc(sizeof(ListGraph));
     (*lg).tot = 0;
-    for (int i = 0; i < MAX_NODE; i++) (*lg).head[i] = -1;
+    for (int i = 0; i < MAX_NODE; i++) (*lg).head[i] = -1, (*lg).indegree[i] = 0;
     return *lg;
 }
 void AddEdge_List(ListGraph *lg, int u, int v, int w)              //add a new edge
 {
     (*lg).e[(*lg).tot] = (edge){v, (*lg).head[u], w};
     (*lg).head[u] = (*lg).tot++;
+    (*lg).indegree[v]++;
 }
 int ShortestPath_List(ListGraph lg, int u, int v)             //shortest path from u to v
 {
@@ -66,4 +67,26 @@ int ShortestPath_List(ListGraph lg, int u, int v)             //shortest path fr
     }
     if (dis[v] == INF) return -1;               //can't reach
     return dis[v];
+}
+void TopoSort(ListGraph lg)               //AOV Network Sort
+{
+    Queue q = CreateQueue();
+    int res[MAX_NODE], cnt = 0;
+    for (int i = 0; i < MAX_NODE; i++)
+        if (!lg.indegree[i] && lg.head[i] != -1) {
+            PushQueue(&q, i);
+            res[cnt++] = i;
+        }
+    while (!IsEmpty(q)) {
+        int u = q.data[q.front];
+        PopQueue(&q);
+        for (int i = lg.head[u]; i != -1; i = lg.e[i].next) {
+            int v = lg.e[i].to;
+            if (--lg.indegree[v] == 0) {
+                res[cnt++] = v;
+                PushQueue(&q, v);
+            }
+        }
+    }
+    for (int i = 0; i < cnt; i++) printf("%d%c", res[i], i == cnt - 1 ? '\n' : ' ');
 }

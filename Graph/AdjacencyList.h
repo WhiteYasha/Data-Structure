2794d88f990b91e0f5d81199ea2192306b14321f
MtJZd <white_yasha@163.com>
2018-11-29 08:56:31 +0800

    Create AdjacencyList.h

---
+++ Graph/AdjacencyList.h
@@ -0,0 +1,37 @@
+//
+//  AdjacencyList.h
+//  Data Structures
+//
+//  Created by 倪思涛 on 2018/11/28.
+//  Copyright © 2018 倪思涛. All rights reserved.
+//
+
+#ifndef AdjacencyList_h
+#define AdjacencyList_h
+
+//adjacency list weighted directed graph (array form)
+
+#include <stdio.h>
+#include <stdlib.h>
+#define INF 0x3f3f3f3f
+#define MAX_NODE 30
+#define MAX_EDGE MAX_NODE * MAX_NODE / 2
+
+typedef struct edge {
+    int to, next, len;
+} edge;
+typedef struct Queue {
+    int data[MAX_EDGE];
+    int front, back;
+} Queue;
+typedef struct ListGraph {
+    edge e[MAX_EDGE];
+    int head[MAX_NODE];
+    int tot;
+} ListGraph;
+
+ListGraph CreateListGraph();               //create a new empty list graph
+void AddEdge_List(ListGraph *lg, int u, int v, int w);              //add a new edge
+int ShortestPath_List(ListGraph lg, int u, int v);             //shortest path from u to v
+
+#endif /* AdjacencyList_h */
